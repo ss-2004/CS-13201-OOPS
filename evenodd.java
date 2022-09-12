204e893ed2d87712ea3aa12dfb4927b7962251c0
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-09-12 11:41:10 +0530

    Part A Q3

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class evenodd
+{
+        public static void main(String args[])
+        {
+                Scanner ss = new Scanner(System.in);
+                int n = ss.nextInt();
+                if(n%2==0)
+			System.out.println("EVEN");
+		else
+			System.out.println("ODD");
+        }
+}
