71c206cc7e0f42080f9d78629dbedc03438c90c4
Shresth Sonkar <93792941+ss-2004@users.noreply.github.com>
2022-09-12 11:40:32 +0530

    Part A Q2

---
+++ circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class circle
+{
+	public static void main(String args[])
+	{
+		Scanner ss = new Scanner(System.in);
+		double r = ss.nextDouble();
+		System.out.println("Area = " + Math.PI*r*r);
+	}
+}
